6b395294fa059aade672ab4651277f72aef2805d
carlotaquintana <796017@unizar.es>
2025-06-26 10:20:52 +0200

    search fixed

--- src/sid/OntView2/common/PaintFrame.java
+++ src/sid/OntView2/common/PaintFrame.java
@@ -1244,6 +1244,7 @@ public class PaintFrame extends Canvas {
                 }
             }
         }
+		getParentFrame().loadSearchCombo();
         compactGraph();
         if (menuVisShapeContext != null) {
             menuVisShapeContext.updateSliderView();
