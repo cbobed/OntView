0c372efad20902b94638a4533c0424cf7770ec6b
cbobed <cbobed@unizar.es>
2026-04-30 08:58:35 +0200

    Added the ontVibe ontology and a simple script to run it in Windows

---
+++ execute.bat
@@ -0,0 +1,7 @@
+echo "Installing pedviz_0.15.jar..."
+mvn install:install-file -Dfile=lib/pedviz_0.15.jar -DgroupId=org.sid.ontview -DartifactId=pedviz -Dversion=0.15 -Dpackaging=jar
+
+echo "Installing KCE-0.0.1-SNAPSHOT.jar..."
+mvn install:install-file -Dfile=lib/kce/KCE-0.0.1-SNAPSHOT.jar -DgroupId=org.sid.ontview -DartifactId=kce -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
+
+mvn clean compile exec:java -Dexec.args="-Dprism.targetvram=2G -Xmx4G -Dprism.order=es2 -Dprism.forceGPU --add-opens javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED"
